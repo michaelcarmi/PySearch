b603e1a1b9ffd74235a83699694bc24aaf4dddf0
Michael Carmi <mrcarmi826@gmail.com>
2020-03-19 13:20:55 -0400

    Added pi and just tweaks not working yet

--- PiSearch.py
+++ PiSearch.py
@@ -1,11 +1,27 @@
 import tkinter as tk
+import math as math
 
 app = tk.Tk()
 app.geometry("300x300")
 
-input_val = ""
+entry = tk.Entry(app)
 
-tk.Entry(app).grid(column = 0, row = 0)
+pi = open("pi.txt", "r")
+
+def button():
+    global pi
+
+    input_val = entry.get()
+    
+    if input_val in pi:
+        print ("Your number is in pi")
+    
+    else:
+        print("no")
+
+entry.grid(column = 0, row = 0)
+tk.Button(app, command = button).grid(column = 0, row = 1)
+
 
 
 app.mainloop()
